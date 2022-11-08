63ee6d01056f4c194c425ce27a67012372f0bf46
willismax <willismax.com@gmail.com>
2022-11-08 15:40:36 +0800

    Fix config.py

--- config.py
+++ config.py
@@ -2,5 +2,6 @@ CHANNEL_ACCESS_TOKEN = ''
 CHANNEL_SECRET = ''
 LINE_USER_ID = ''
 IMGURE_CLIENT_ID = ''
+HACKMD_API_TOKEN = ''
 TODO_NOTE_ID = '' #  paste your fixed TODO note id, len == 22
 TEMP_NOTE_ID = '' # paste your fixed TEMP note id, len == 22
